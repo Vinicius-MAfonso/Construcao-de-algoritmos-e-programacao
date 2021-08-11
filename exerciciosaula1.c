119905d103d0d99e7b934645416af428c2ee52c7
fafamilggd <vinicius.mariano.afonso@gmail.com>
2021-08-10 22:57:48 -0300

    Introdução a programação

---
+++ exerciciosaula1.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+void main(){
+setlocale(LC_ALL, "Portuguese");
+// int nota1 = 0;
+// int nota2 = 0;
+// int nota3 = 0;
+// int nota4 = 0;
+
+// printf("Digite a nota 1:");
+// scanf("%d",&nota1);
+// printf("Digite a nota 2:");
+// scanf("%d",&nota2);
+// printf("Digite a nota 3:");
+// scanf("%d",&nota3);
+// printf("Digite a nota 4:");
+// scanf("%d",&nota4);
+// int media = ((nota1+nota2+nota3+nota4)/4);
+// printf("%d",media);
+
+// int valorHora, qtdHoras, salario;
+// printf("Digite quanto voce ganha por hora:");
+// scanf("%d", &valorHora);
+// printf("Digite quantas horas voce trabalha no mes:");
+// scanf("%d", &qtdHoras);
+// salario = valorHora * qtdHoras;
+// printf("Seu salario mensal é de: %d \n", salario);
+
+// João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho. 
+// Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) 
+// deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa que leia a variável peso (peso de peixes) e calcule o excesso. 
+// Gravar na variável excesso a quantidade de quilos além do limite e na variável multa o valor da multa que João deverá pagar. 
+// Imprima os dados do programa com as mensagens adequadas.
+
+
+
+
+
+}
+
--- primeiroprogramaemC.c
+++ primeiroprogramaemC.c
@@ -1,6 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <locale.h>
 void main(){
-    printf("Hello world!");
+setlocale(LC_ALL, "Portuguese");
+/* 
+Tipos de variaveis(int,float,double,string,char,bool)
+int --> 0-1-2
+float --> 0,67 - 13,56
+double --> 0,67 - 13,56
+string --> "Alciomar"
+char --> "SP","M"
+bool -- > TRUE,FALSE,0,1
+*/  
+    int num;
+    
+    printf("Digite um numero: ");
+    scanf("%d",&num);
+    
+    printf("O número digitada foi %d",num);
 }
